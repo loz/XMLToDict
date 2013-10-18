af8599a4a95993db081f6614841501d447cc3cf2
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2013-10-18 07:51:02 +0100

    Initial Commit

---
+++ XMLToDict/XMLToDict.h
@@ -0,0 +1,13 @@
+//
+//  XMLToDict.h
+//  XMLToDict
+//
+//  Created by Jonathan Lozinski on 18/10/2013.
+//  Copyright (c) 2013 Jonathan Lozinski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLToDict : NSObject
+
+@end
---
+++ XMLToDict/XMLToDict.m
@@ -0,0 +1,13 @@
+//
+//  XMLToDict.m
+//  XMLToDict
+//
+//  Created by Jonathan Lozinski on 18/10/2013.
+//  Copyright (c) 2013 Jonathan Lozinski. All rights reserved.
+//
+
+#import "XMLToDict.h"
+
+@implementation XMLToDict
+
+@end
